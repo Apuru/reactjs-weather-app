e8b50216d265894366e510bc46b74b1611cb1f90
Apuru <alex.toronto@live.com>
2018-06-19 14:48:03 -0400

    small update

--- src/weather-components/Buttons.js
+++ src/weather-components/Buttons.js
@@ -15,8 +15,6 @@ class Weather extends React.Component {
       minTemp: undefined,
       maxTemp: undefined
     }
-    // bind the updateState function to this so we can call it later to change
-    updateState = updateState.bind(this)
     resolveSearch = resolveSearch.bind(this)
   }
 
@@ -34,6 +32,12 @@ class Weather extends React.Component {
       let temp3 = weather.main.temp_max
       resolveSearch(temp1, temp2, temp3)
     })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.city !== prevProps.city) {
+      this.handleSearch()
+    }
   }
 
   handleSearch() {
@@ -65,7 +69,7 @@ class Weather extends React.Component {
     else {
       return (
         <div>
-          <h1>Today&#39;s weather in {this.state.city}</h1>
+          <h1>Today&#39;s weather in {this.props.city}</h1>
           Current temperature: {this.state.curTemp} C<br />
           Min temperature: {this.state.minTemp} C<br />
           Max temperature: {this.state.maxTemp} C<br />
@@ -73,13 +77,6 @@ class Weather extends React.Component {
       );
     }
   }
-}
-
-// a function to update the other component
-var updateState = function (text) {
-  // unbound apparently, this is ridiculously important
-  // this.setState( {city: text}, this.handleSearch() )
-  this.setState( {city: text}, () => this.handleSearch() )
 }
 
 var resolveSearch = function (temp1, temp2, temp3) {
@@ -95,52 +92,49 @@ export default class Buttons extends React.Component {
   constructor() {
     super();
     this.state = {
-      active1: true,
-      active2: false,
-      active3: false,
+      city: "Toronto",
     };
   }
 
-  componentDidMount() {
+  // a function to update the other component
+  updateState(text) {
+    // unbound apparently, this is ridiculously important
+    // this.setState( {city: text}, this.handleSearch() )
+    this.setState( {city: text} )
   }
 
   toronto() {
-    this.setState({active1: !this.state.active1})
-    this.setState({active2: false})
-    this.setState({active3: false})
-    updateState("Toronto")
+    this.updateState("Toronto")
   }
 
   montreal() {
-    this.setState({active1: false})
-    this.setState({active2: !this.state.active2})
-    this.setState({active3: false})
-    updateState("Montreal")
+    this.updateState("Montreal")
   }
 
   ottawa() {
-    this.setState({active1: false})
-    this.setState({active2: false})
-    this.setState({active3: !this.state.active3})
-    updateState("Ottawa")
+    this.updateState("Ottawa")
   }
 
   render() {
-    // toggleable classes for each button
-    let btn_class1 = this.state.active1 ? "clickedButton" : "unclickedButton";
-    let btn_class2 = this.state.active2 ? "clickedButton" : "unclickedButton";
-    let btn_class3 = this.state.active3 ? "clickedButton" : "unclickedButton";
+    var btn_class = (cityButton) => {
+      if (this.state.city == cityButton) {
+        return "clickedButton"
+      } else {
+        return "unclickedButton"
+      }
+    }
 
     return (
       <div>
         <button
-        className={btn_class1}
+        className={btn_class("Toronto")}
         onClick={this.toronto.bind(this)}>
+
         Toronto
         </button>
 
         <button
-        className={btn_class2}
+        className={btn_class("Montreal")}
         onClick={this.montreal.bind(this)}>
         Montreal</button>
 
@@ -149,9 +143,9 @@ export default class Buttons extends React.Component {
         onClick={this.ottawa.bind(this)}>
         Ottawa</button>
 
-        <Weather />
+        <Weather city={this.state.city} />
 
       </div>
     );
   }
-}
+} // Monday 3pm
